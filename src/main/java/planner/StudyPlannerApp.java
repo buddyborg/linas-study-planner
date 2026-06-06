f61cfeeec81b97f277fd304c187b14ba236af5d8
Lina <buddyborg99@gmail.com>
2026-06-06 12:57:23 -0600

    Added task creation form with validation

--- src/main/java/planner/StudyPlannerApp.java
+++ src/main/java/planner/StudyPlannerApp.java
@@ -2,12 +2,11 @@ package planner;
 
 import javafx.application.Application;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.control.ListView;
+import javafx.scene.control.*;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
+import java.time.LocalDate;
 import java.util.ArrayList;
 
 /**
@@ -21,6 +20,8 @@ public class StudyPlannerApp extends Application {
 
     private TaskManager manager;
     private FileHandler fileHandler;
+    private ListView<String> taskListView; //ListView = horizontal or vertical list of items that user can select/interact with
+
     public static void main(String[] args) {
         launch(args);
     }
@@ -57,7 +58,7 @@ public class StudyPlannerApp extends Application {
     public void showTaskList(Stage stage){
 
         Label titleLabel = new Label("Tasks:");
-        ListView<String> taskListView = new ListView<>(); //ListView = horizontal or vertical list of items that user can select/interact with
+        taskListView = new ListView<>();
 
         Button backBtn = new Button("Back to Dashboard");
         backBtn.setOnAction(event -> showDashboard(stage));
@@ -65,9 +66,7 @@ public class StudyPlannerApp extends Application {
         Button addTaskBtn = new Button("Add New Task");
         addTaskBtn.setOnAction(event -> showAddTaskWindow());
 
-        for(Task task: manager.getTasks()){
-            taskListView.getItems().add(task.getTitle()); //Task Object -> title text
-        }
+        refreshTaskList();
 
         VBox vbox = new VBox(); //vbox = layout pane that arranges its child nodes in vertical column from top to bottom
         vbox.getChildren().addAll(titleLabel, taskListView, backBtn, addTaskBtn);
@@ -79,13 +78,73 @@ public class StudyPlannerApp extends Application {
         stage.setOnCloseRequest(event -> fileHandler.saveTasks(manager.getTasks())); //when close happens, save tasks
     }
 
+    public void refreshTaskList(){
+        taskListView.getItems().clear(); //clears taskListView
+
+        for(Task task: manager.getTasks()){
+            taskListView.getItems().add(task.toString()); //Task Object -> text
+        }
+
+    }
+
     public void showAddTaskWindow(){
 
         Stage addTaskStage = new Stage();
-        Label label = new Label("Add Task Screen");
+        Label topLbl = new Label("Add New Task");
+        Label titleLbl = new Label("Task Name:");
+        TextField addTaskTitle = new TextField();
+        Label dateLbl = new Label("Due Date:");
+        DatePicker addDueDate = new DatePicker();
+        Label priorityLbl = new Label("Priority:");
+        ComboBox<Priority> addPriority = new ComboBox<>();
+        addPriority.getItems().addAll(Priority.LOW, Priority.MEDIUM, Priority.HIGH); //adds into combo box's list
+        addPriority.setValue(Priority.MEDIUM);
+        Label statusLbl = new Label("Completed?:");
+        CheckBox addCompletedStatus = new CheckBox("Complete");
+        Button addTaskBtn = new Button("Add Task");
+        addTaskBtn.setOnAction(event -> {
+            String title = addTaskTitle.getText().trim(); //using trim so that spaces don't get stored
+            LocalDate dueDate = addDueDate.getValue();
+            Priority priority = addPriority.getValue();
+            boolean completed = addCompletedStatus.isSelected();
+
+            String errorMessage = "";
+
+            if(title.isBlank()){
+                errorMessage += "Please enter a task name.\n";
+            }
+
+            if(dueDate == null){
+                errorMessage += "Please choose a due date.";
+            }
+
+            if (!errorMessage.isEmpty())  {
+                Alert alert = new Alert(Alert.AlertType.ERROR);
+
+                alert.setTitle("Missing Task Information");
+                alert.setHeaderText("Could not add task");
+                alert.setContentText(errorMessage);
+                alert.showAndWait();
+
+                return; // Stops the button action so the bad task does not get created or saved
+            }
+
+            Task newTask = new Task(title, dueDate, priority, completed);
+            manager.addTask(newTask);
+
+            refreshTaskList();
+
+            fileHandler.saveTasks(manager.getTasks());
+
+            addTaskStage.close();
+
+        });
+        Button cancelBtn = new Button("Cancel");
+        cancelBtn.setOnAction(event -> addTaskStage.close());
 
         VBox vbox = new VBox();
-        vbox.getChildren().addAll(label);
+        vbox.getChildren().addAll(topLbl, titleLbl, addTaskTitle, dateLbl, addDueDate, priorityLbl, addPriority,
+                statusLbl, addCompletedStatus, addTaskBtn, cancelBtn);
 
         Scene scene = new Scene(vbox, 300, 500);
 
